1e58fa0ad1ba31378c7d2d7eb7d734e4302f8b78
ihgs <hgs.dev@gmail.com>
2024-04-27 08:06:23 +0900

    add alarm

--- src/app/timer/MyTimer.tsx
+++ src/app/timer/MyTimer.tsx
@@ -2,8 +2,10 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from 'next/navigation'
 import { TimerChart } from "../vega/Timer";
-import { Button, Stack, Typography } from "@mui/material";
+import { Button, FormControlLabel, FormGroup, Stack, Switch, Typography } from "@mui/material";
 import { BarTimerChart } from "../vega/BarTimer";
+import * as Tone from "tone"
+
 
 
 interface Milestone {
@@ -22,6 +24,11 @@ export default function MyTimer() {
   const [show, setShow] = useState<boolean>(false)
   const [showBar, setShowBar] = useState<boolean>(false)
   const [graphSize, setGraphSize] = useState<number>(300)
+  const [finished, setFinished] = useState<boolean>(true)
+
+
+
+  // let finished = true;
 
   useEffect(() => {
     setGraphSize(Math.min(window.innerHeight, window.innerWidth))
@@ -36,6 +43,19 @@ export default function MyTimer() {
       setData(tmpData)
     }
   }, [points])
+
+
+  const alertListeners = {baseTimer: (name:string, value:any)=>{
+    if (!finished && value<0){
+      const synth = new Tone.Synth().toDestination();
+      const seq = new Tone.Sequence((time, note)=>{
+        synth.triggerAttackRelease(note, '16n', time)
+      }, ["B5", "B5"]).start();
+      seq.loop = false;
+      setFinished(true)
+      Tone.Transport.start()
+    }
+  }}
   return (
 
     <Stack sx={{ paddingTop: 6 }} direction={"column"} gap={3} alignItems={"center"} >
@@ -48,8 +68,10 @@ export default function MyTimer() {
           <Stack direction={'row'} gap={2}>
             <Button variant="contained" onClick={() => setShow(true)}>Start circle timer ({min} 分)</Button>
             <Button variant="contained" onClick={() => setShowBar(true)}>Start bar timer ({min} 分)</Button>
-
           </Stack>
+          <FormGroup>
+            <FormControlLabel control={<Switch onChange={()=>{setFinished(false)}}/>} label="Alarm" />
+          </FormGroup>
         </>
 
       }
@@ -57,14 +79,14 @@ export default function MyTimer() {
       {
         show &&
         <>
-          <TimerChart actions={false} data={{ table: data, timer: [{ min }] }} width={graphSize * 0.8} height={graphSize * 0.8} />
+          <TimerChart actions={false} data={{ table: data, timer: [{ min }] }} width={graphSize * 0.8} height={graphSize * 0.8} signalListeners={alertListeners} />
 
         </>
 
       }
       {
         showBar &&
-        <BarTimerChart actions={false} data={{ table: data, timer: [{ min }] }} width={window.innerWidth * 0.8} height={window.innerHeight * 0.8} />
+        <BarTimerChart actions={false} data={{ table: data, timer: [{ min }] }} width={window.innerWidth * 0.8} height={window.innerHeight * 0.8} signalListeners={alertListeners}/>
       }
 
 
